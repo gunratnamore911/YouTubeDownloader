97a00fd2708f9247695b7f93f6e4b2141544f57b
gunratnamore911 <gunratnamore@gmail.com>
2021-04-06 01:37:29 +0530

    final sendoff

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const colors = require("colors");
 const app = express();
 const ytdl = require("ytdl-core");
 const cors = require("cors");
+const path = require("path");
 
-const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ extended: false }));
 
 app.use("/api/videos", require("./routes/api/getvideos"));
-
-app.get("/", (req, res) => {
-  res.send("<h1>hi Gunratna More!!!</h1>");
-});
 
 app.get("/download", (req, res) => {
   var URL = req.query.URL;
@@ -24,6 +20,15 @@ app.get("/download", (req, res) => {
   }).pipe(res);
 });
 
+//serving static assests
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`.cyan.bold);
 });
